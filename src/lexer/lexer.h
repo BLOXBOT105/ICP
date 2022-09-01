4a54c7316bc56fcc27c2d7277091236a4f5652cf
Nick Geiser <zitekdev@gmail.com>
2022-09-01 11:58:57 -0400

    update msg

--- src/lexer/lexer.h
+++ src/lexer/lexer.h
@@ -2,4 +2,9 @@
 
 char** LexContents(char *script_contents); 
 
-char** __Handle_Includes__(char* script_contents);
+/*
+ This function will read through the program after it is lexe
+ and build all files that get included before it builds this one which creates 
+ no errors
+*/
+char** __Handle_Includes__(char** lexed_program);
