194143206b71365c36c371e310cf70429295ed16
MrExplosive <68560474+BLOXBOT105@users.noreply.github.com>
2022-09-07 09:57:27 -0400

    Update asmfile.asm

--- ideas/asmfile.asm
+++ ideas/asmfile.asm
@@ -1,12 +1,12 @@
-main:
+main: ;; function main with 0 args
    PUSH %BP
    MOV  %SP, %BP
-@main_body:
+@main_body: ;; contents of main
    SUB  %SP, $4, %SP
    MOV  $1, -4(%BP) ;; $retun value
    MOV  -4(%BP), %13
    JMP  @main_exit
-@main_exit:
+@main_exit: ;; return
    MOV  %BP, %SP
    POP  %BP
    RET 
